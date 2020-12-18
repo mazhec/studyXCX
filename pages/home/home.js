d1baff453b8c372a51927ba87aa2476ff8809096
马喆昌 <7493508+mazhechang@user.noreply.gitee.com>
2020-12-18 16:50:23 +0800

    知识点3

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,39 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'XiaoMaZai',
+    age: 120,
+    students: [{
+        id: 1,
+        name: '姬大德',
+        age: 20
+      },
+      {
+        id: 2,
+        name: '曹德',
+        age: 22
+      },
+      {
+        id: 3,
+        name: '曹龙',
+        age: 24
+      }
+    ],
+    counter: 0
   },
+  handleBtnClick() {
+    // console.log('-----------')
+    // this.data.counter += 1
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
+  handleBtnClick2() {
+    // console.log('-----------')
+    // this.data.counter += 1
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
 })
