a0fe816e7e8a5c719c80f2a86442929077e093d4
马喆昌 <7493508+mazhechang@user.noreply.gitee.com>
2020-12-23 14:20:21 +0800

    事件相关知识点

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,75 @@ Page({
    * 页面的初始数据
    */
   data: {
+    titles: ['衣服', '裤子', '鞋子']
+  },
 
+  handleBtnClick() {
+    console.log('按钮发送点击')
+  },
+
+  handleTouchStart(e) {
+    // console.log(e.touches[0].pageX)
+  },
+
+  handleTouchMove(e) {
+    console.log(e)
+  },
+  handleTouchEnd(e) {
+    // console.log('鼠标离开')
+  },
+  handleTouchTap(e) {
+    // console.log('鼠标点击后马上离开')
+  },
+  handleLongPress() {
+    // console.log('长按超过350毫秒')
+  },
+
+  hangleEventClick(e) {
+    console.log(e)
+  },
+
+  hangleEventEnd(e) {
+    console.log('hangleEventEnd', e)
+  },
+
+  hangleInner(e) {
+    // console.log(e)
+  },
+
+  handleOuter(e) {
+    console.log(e)
+  },
+
+  handleItemClick(e) {
+    // console.log(e.currentTarget.dataset)
+    const dataset = e.currentTarget.dataset
+    const index = dataset.index
+    const item = dataset.item
+    console.log(item, index)
+  },
+
+  // 事件冒泡和事件捕获
+
+  handleCaptureView1() {
+    console.log('handleCaptureView1')
+  },
+  handleBindView1() {
+    console.log('handleBindView1')
+  },
+
+  handleCaptureView2() {
+    console.log('handleCaptureView2')
+  },
+  handleBindView2() {
+    console.log('handleBindView2')
+  },
+
+  handleCaptureView3() {
+    console.log('handleCaptureView3')
+  },
+  handleBindView3() {
+    console.log('handleBindView3')
   },
 
   /**
