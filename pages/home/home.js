6dcdd42e6a85aea6a9355f5b2884a617bffe6b9c
马喆昌 <7493508+mazhechang@user.noreply.gitee.com>
2020-12-25 14:20:18 +0800

    展示窗口和分享按钮

--- pages/home/home.js
+++ pages/home/home.js
@@ -8,59 +8,121 @@ Page({
 
   },
 
+  handleShowToast() {
+    wx.showToast({
+      title: '义薄云天',
+      icon: 'loading',
+      duration: 2000,
+      image: '/assets/profile/app.png',
+      mask: true,
+      success: function() {
+        console.log('showToast调用成功')
+      },
+      fail: function() {
+        console.log('showToast调用失败')
+      },
+      complete: function() {
+        console.log('showToast被调用')
+      }
+    })
+  },
+
+  handleShowModal() {
+    wx.showModal({
+      title: '义薄云天',
+      content: '敦厚纯善',
+      success: function(res) {
+        if (res.confirm) {
+          console.log('用户点击了确定')
+        } else if (res.cancel) {
+          console.log('用户点击了取消')
+        }
+      }
+    })
+  },
+
+  handleShowLoading() {
+    wx.showLoading({
+      title: '正在加载',
+      mask: true,
+      success: function() {
+        setTimeout(function() {
+          wx.hideLoading()
+        }, 2000)
+      }
+    })
+  },
+
+  handleActionSheet() {
+    wx.showActionSheet({
+      alertText: '123',
+      itemList: ['A', 'B', 'C'],
+      success(res) {
+        console.log(res)
+      },
+      fail(res) {
+        console.log(res)
+      }
+    })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function(options) {
+    return {
+      title: '义薄云天楚魔王',
+      path: '/pages/about/about',
+      imageUrl: '/assets/profile/message.png'
+    }
   }
 })
