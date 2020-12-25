d34fb6d5504e13d36705d376a21b0d782e0cc175
马喆昌 <7493508+mazhechang@user.noreply.gitee.com>
2020-12-25 12:04:02 +0800

    网络请求及封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,5 @@
 // pages/home/home.js
+import request from "../../service/network.js"
 Page({
 
   /**
@@ -11,56 +12,75 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    // 1.原生方式发送网络请求
+    this.get_data_origin()
 
+    // 2.使用封装的request发送网络请求
+    request({
+      url: 'http://152.136.185.210:7878/api/m5/recommend'
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+  },
+
+  get_data_origin() {
+    // wx.request({
+    //   url: 'http://152.136.185.210:7878/api/m5/recommend',
+    //   success: (res) => {
+    //     console.log(res)
+    //   }
+    // })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
